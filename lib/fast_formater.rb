fc244876ca3e3262eeb0fe306c2118130b498f3d
João Marcus de Lemos Fernandes <jmpierrotfernandes@hotmail.com>
2016-02-18 19:03:44 -0200

    Formate method edited

--- lib/fast_formater.rb
+++ lib/fast_formater.rb
@@ -2,70 +2,66 @@ require "fast_formater/version.rb"
 require 'json'
 
 module FastFormater
-  class Formater
+	class Formater
 
-    def getJson(h)
+		def getJson(h)
+			if @json_format != nil then return @json_format end
 
-      if @json_format != nil then return @json_format end
+			formating(h, { json: true })
+			@json_format
+		end
 
-      formating(h, { json: true })
-      @json_format
-    end
+		def getXml(h)
+			if @xml_format != nil then return @xml_format end
 
-    def getXml(h)
+			formating(h, { xml: true })
+			@xml_format
+		end
 
-      if @xml_format != nil then return @xml_format end
+		def makeJson(h)
+			if @json_format != nil then getJson(h) end 
+			f = File.open("format.json", "w")
+			f.puts @json_format
+			f.close
+		end
 
-      formating(h, { xml: true })
-      @xml_format
-    end
+		def makeXml(h)
+			if @xml_format != nil then getXml(h) end
+			f = File.open("format.xml", "w")
+			f.puts @xml_format
+			f.close
+		end
 
-    def makeJson(h)
-		if @json_format != nil then getJson(h) end 
-		f = File.open("format.json", "w")
-		f.puts @json_format
-		f.close
-    end
+		private
 
-    def makeXml(h)
-    		if @xml_format != nil then getXml(h) end
-    		f = File.open("format.xml", "w")
-		f.puts @xml_format
-		f.close
-      end
+			def formating(hasher ,params)
+				@rb_hash = hasher
 
-    private
+				if params[:json]
+				@json_format = @rb_hash.to_json
+				end
 
-      def formating(hasher ,params)
-        @rb_hash = hasher
-
-        if params[:json]
-          @json_format = @rb_hash.to_json
-        end
-
-        if params[:xml]
-          @xml_format = to_xml(@rb_hash)
-        end
-      end
+				if params[:xml]
+				@xml_format = to_xml(@rb_hash)
+				end
+			end
 
 
-      def to_xml(hasher)
+			def to_xml(hasher)
+				my_xml  = '<?xml version="1.0" encoding="UTF-8" ?>'
 
-        my_xml  = '<?xml version="1.0" encoding="UTF-8" ?>'
+				if hasher[:title] == nil then hasher[:title] = 'unknown' end
 
-        if hasher[:title] == nil then hasher[:title] = 'unknown' end
+				my_xml += "\n<#{hasher[:title]}>"
 
-        my_xml += "\n<#{hasher[:title]}>"
+				hasher.each do |tag, content|
+					if tag == :title then break end
+					my_xml += "\n   <#{tag}>#{content}</#{tag}>"
+				end
 
-        hasher.each do |tag, content|
-        	if tag == :title then break end
-          my_xml += "\n   <#{tag}>#{content}</#{tag}>"
-        end
+				my_xml += "\n</#{hasher[:title]}>"  
 
-      my_xml += "\n</#{hasher[:title]}>"  
-
-        return my_xml
-      end
-
-  end
+				return my_xml
+			end
+	end
 end
